7be1f32084a6622484895c09b0a75feadad6832a
Richy <70558343+Varith97@users.noreply.github.com>
2020-11-16 10:10:40 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,150 @@
+//const mysql = require('mysql');
+const mysql = require('promise-mysql');
+const express = require('express')
+const app = express()
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use('/', router);
+
+
+app.get('/', async function(req, res) {
+  res.send('Hello World!')
+});
+
+app.get('/test', async (req, res) => {
+    makeQuery('select * from test_login')
+    .then(data => res.json(data))
+    .catch(err => handleErr(err, res, null));
+    
+});
+
+app.get('/login', async (req, res) => {
+    console.log("attempting to login");
+//    console.log(req);
+    var phonenumReq = req.query.phone_num;
+    var passwordReq = req.query.password;
+
+    makeQuery("SELECT username FROM test_login WHERE phone_num = ? AND password = ? limit 1", [phonenumReq, passwordReq])
+    .then(data => {
+        if (data) {
+//            let result = {
+//                username: data[0].username
+//            }
+            res.send(data[0].username);
+        }
+    }).catch(err => handleErr(err, res, null));
+});
+
+app.post('/register', async (req, res) => {
+    var today = new Date();
+    console.log("attemping to signup");
+    const passwordHash = bcrypt.hashSync(req.body.password, 10);
+    var users = {
+        "customer_name" : req.body.name,
+        "customer_surname" : req.body.surname,
+        "customer_password" : passwordHash,
+        //"customer_birthdate" :today.toISOString().slice(0, 19).replace('T', ' '),
+        "customer_phone_num" : req.body.phone_num,
+        "customer_primary_address" : req.body.primary_address
+    };
+
+    var phone_numReq = req.body.phone_num;
+    makeQuery("SELECT customer_phone_num FROM customers WHERE customer_phone_num = ?", phone_numReq)
+    .then(result => {
+        if (!result || !result[0]) {
+            makeQuery('INSERT INTO customers SET ?', users)
+            .then(results => {
+                res.json({
+                    status: true,
+                    data: results,
+                    message: 'user registered sucessfully'
+                });
+            }).catch(err => handleErr(err, res, 'there are some error with query'));
+        }
+        else {
+            res.send("phone number already in used");
+            console.log(result);
+        }
+    }).catch(err => handleErr(err, res, null));
+});
+
+
+//app.get('/username', function(req, res,next){
+//  con.query("SELECT username FROM member",function (err, result, fields){
+//    if (err) throw err;
+//    console.log(result);
+//    res.type("json");
+//    res.send(result);
+//  });
+//});
+
+async function makeQuery(qry, args) {
+  let db;
+  try {
+    db = await getDB();
+    let data;
+    if (args != null) {
+      data = await db.query(qry, args);
+    } else {
+      data = await db.query(qry);
+    }
+    db.end();
+    return data;
+  } catch (err) {
+    if (db) { // undefined if an error occurred in getDB()
+      db.end();
+    }
+    console.log(err);
+    throw err; // to bubble up an error to be caught in the function that called makeQuery().
+  }
+}
+
+async function getDB() {
+  let database = await mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+//    password: "your_password",
+    database: "test_db"
+  });
+  return database;
+}
+
+//function checkStatus(response) {
+//  if (response.ok) {
+//    return response;
+//  } else {
+//     throw Error("Error in request: " + response.statusText);
+//  }
+//}
+
+/**
+ * Handle error when unable to read source file or invalid get parameters.
+ * @param {error} err - error information.
+ * @param {response} res - response object to send data.
+ * @param {string} msg - err message.
+ */
+function handleErr(err, res, msg) {
+  console.log(err);
+  if (err.code === "ENOENT") {
+    res.type('text');
+    res.status(400).send(msg);
+  } else {
+    res.type('text');
+    res.status(500).send('Something went wrong on the server, try again later.');
+  }
+}
+
+app.use(express.static('public'));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log("listening on port 8000");
+});
---
+++ public/test/script.js
@@ -0,0 +1,187 @@
+"use strict";
+
+const baseUrl = "http://localhost:8000";
+
+window.addEventListener("load", () => {
+    /* First Page Login/Signup - Animation */
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container_log = document.getElementById('container_log');
+
+    signUpButton.addEventListener('click', () => {
+        container_log.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+        container_log.classList.remove("right-panel-active");
+    });
+
+    /* User account sign up/sign in */
+    document.getElementById("signin-btn").addEventListener("click", () => {
+        var phoneInput = document.querySelector("input[type='phone'].signin");
+        var passwordInput = document.querySelector("input[type='password'].signin");
+        
+        if (validLogin(phoneInput, passwordInput)) {
+            login(phoneInput.value, passwordInput.value);
+        }
+    });
+
+    document.getElementById("signup-btn").addEventListener("click", () => {
+        var phoneInput = document.querySelector("input[type='phone'].signup");
+        var passwordInput = document.querySelector("input[type='password'].signup");
+        var repasswordInput = document.querySelector("input[type='repassword'].signup");
+        var emailInput = document.querySelector("input[type='email'].signup");
+        var usernameInput = document.querySelector("input[type='username'].signup");
+
+        if (validCreateAccount(usernameInput, phoneInput, passwordInput, repasswordInput, emailInput)) {
+            createAccount(usernameInput.value, phoneInput.value, passwordInput.value, emailInput.value);
+        }
+    });
+});
+
+function validLogin(phoneInput, passwordInput) {
+    let error = false;
+    if (!validatePhoneNum(phoneInput.value)) {
+//        console.log("Invalid phone number");
+        error = true;
+        setErrorInput(phoneInput, true);
+    } else {
+        setErrorInput(phoneInput, false);
+    }
+    
+    if (!validatePassword(passwordInput.value)) {
+//        console.log("Invalid password format");
+        error = true;
+        setErrorInput(passwordInput, true);
+    } else {
+        setErrorInput(passwordInput, false);
+    }
+    return !error;
+}
+
+function validCreateAccount(usernameInput, phoneInput, passwordInput, repasswordInput, emailInput) {
+    let error = false;
+    if (!usernameInput.value) {
+        error = true;
+        setErrorInput(usernameInput, true);
+    } else {
+        setErrorInput(usernameInput, false);
+    }
+    
+    if (!validatePhoneNum(phoneInput.value)) {
+//        console.log("Invalid phone number");
+        error = true;
+        setErrorInput(phoneInput, true);
+    } else {
+        setErrorInput(phoneInput, false);
+    }
+    
+    if (!validatePassword(passwordInput.value)) {
+//        console.log("Invalid password format");
+        error = true;
+        setErrorInput(passwordInput, true);
+    } else {
+        setErrorInput(passwordInput, false);
+    }
+    
+    if (!repasswordInput.value || passwordInput.value != repasswordInput.value) {
+//        console.log("Passwords don't match");
+        error = true;
+        setErrorInput(repasswordInput, true);
+    } else {
+        setErrorInput(repasswordInput, false);
+    }
+    
+    if (emailInput.value && !validateEmail(emailInput.value)) {
+//        console.log("Invalid email");
+        error = true;
+        setErrorInput(emailInput, true);
+    } else {
+        setErrorInput(emailInput, false);
+    }
+    return !error;
+}
+
+async function createAccount(username, phone, password, email) {
+    //TODO: 
+    console.log("creating account: " + phone + password + email);
+    let form = new FormData();
+    form.append("username", username);
+    form.append("phone_num", phone);
+    form.append("password", password);
+    form.append("email", email);
+    
+    fetchPost("/register", form, (resp) => {
+        console.log(resp.json());
+
+    });
+}
+
+async function login(phone, password) {
+    console.log("logging in: " + phone + password);
+    //TODO: 
+    fetchGet("/login?phone_num=" + phone + "&password=" + password, (data) => {
+        console.log(data);
+    });
+}
+
+function setErrorInput(elm, error) {
+    if (error) {
+        elm.style.border = "1px solid red";
+    } else {
+        elm.style.border = "none";
+    }
+}
+
+function validatePhoneNum(num) {
+    return num.match(/^0\d{8,9}$/g);
+}
+
+function validatePassword(password) {
+//    return password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g);
+    return password != "";
+}
+    
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+async function fetchGet(url, callBack) {
+    fetch(baseUrl + url)
+      .then(checkStatus)
+      .then(resp => resp.text())
+      .then(callBack)
+      .catch(handleErr);
+}
+
+function fetchPost(url, form, callBack) {
+//    let params = new FormData(form);
+    fetch(baseUrl + url, { method : "POST", body : form })
+      .then(checkStatus)
+      .then(resp => resp.text())
+      .then(callBack)
+      .catch(handleErr);
+}
+
+/**
+  * Checking status of server from given response.
+  * Throw error if server not ready.
+  * @param {Promise} response - response from server status.
+  * @return {Promise} Promise onject.
+ */
+function checkStatus(response) {
+    if (!response.ok) { // response.status >= 200 && response.status < 300
+        throw Error("Error in request: " + response.statusText);
+    }
+    return response;
+}
+
+/**
+  * Handle error from fetching.
+  * @param {error} err - error from fetching data.
+*/
+function handleErr(err) {
+    //showView('error');
+    console.log(err);
+}
